2ad4a4ab549e3f4bff77b8094a73453e66d20854
Bloodray636 <d.korolev2003666@bk.ru>
2025-10-10 03:51:50 +0600

    Реализован блок До / После

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,3 @@
-window.onload = function () {
-  document.body.classList.add('loaded_hiding');
-  window.setTimeout(function () {
-    document.body.classList.add('loaded');
-    document.body.classList.remove('loaded_hiding');
-  }, 500);
-};
-
 $("#owl-demo-1").owlCarousel({
   loop:true,
   nav: false,
