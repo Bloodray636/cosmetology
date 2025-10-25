91869b33ed4b0bc4a6f28ff4c2bd953452a6db93
Bloodray636 <d.korolev2003666@bk.ru>
2025-10-25 14:41:54 +0600

    Добавлены ссылки на соц.сети, изменен радиус округления изображений, изменен адаптив услуг

--- dist/js/tabs.js
+++ dist/js/tabs.js
@@ -481,7 +481,7 @@ tabs.forEach(tab => {
 
     tab.services.forEach(({ image, title, price, link }) => {
       const col = document.createElement("div");
-      col.className = "col-6 col-md-4 col-lg-3 mb-5";
+      col.className = "col-12 col-sm-6 col-md-4 col-lg-3 mb-5";
       col.innerHTML = `
         <div class="card h-100 border-0 p-0 p-3">
           <div class="card-body p-0">
